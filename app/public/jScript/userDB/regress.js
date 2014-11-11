ee6282743c2981de8c5417a8a5c7c488dd0ef2af
Matthaus <mjwolff@umass.edu>
2014-11-11 12:44:11 -0500

    Minor edit

--- app/public/jScript/userDB/regress.js
+++ app/public/jScript/userDB/regress.js
@@ -14,11 +14,6 @@ var server = http.createServer(function(req, res) {
       // no error occurred, continue with the request
       if(!err) return false;
 
-      // An error occurred, remove the client from the connection pool.
-      // A truthy value passed to done will remove the connection from the pool
-      // instead of simply returning it to be reused.
-      // In this case, if we have successfully received a client (truthy)
-      // then it will be removed from the pool.
       done(client);
       res.writeHead(500, {'content-type': 'text/plain'});
       res.end('An error occurred');
@@ -40,10 +35,9 @@ var server = http.createServer(function(req, res) {
         // return the client to the connection pool for other requests to reuse
         done();
         res.writeHead(200, {'content-type': 'text/plain'});
-        res.end('You are visitor number ' + result.rows[0].count);
       });
     });
   });
 })
 
-server.listen(3001)
+server.listen(1000);
