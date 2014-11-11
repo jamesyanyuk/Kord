12667f45381d4b9e05c3d27fcd072298855e616f
jkuk <jkuk@umass.edu>
2014-11-11 13:00:17 -0500

    adding libs folder, javascripts folder, adding beginnings of Board, Room, RoomDB js files

---
+++ app/public/jScript/Room.js
@@ -0,0 +1,9 @@
+function Room(boards, url, password, admins, members, chatLog) {
+	this.boards = boards;
+	this.url = url;
+	this.password = password;
+	this.admins = admins;
+	this.members = members;
+	this.chatLog = chatLog;
+}
+
---
+++ app/public/jScript/RoomDB.js
@@ -0,0 +1,15 @@
+function RoomDB() {
+	
+}
+
+exports.createRoom() = function () {
+	
+}
+
+exports.getRoomByID() = function () {
+	
+}
+
+exports.updateRoom = function () {
+	
+}
