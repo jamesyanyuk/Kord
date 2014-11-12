ddc5a9fe32f47045fcba63baaf7af79a1e7231ec
jkuk <jkuk@umass.edu>
2014-11-11 20:24:24 -0500

    uploading bare bones for the database -- IT IS NOT READY TO GO -- USE IT ONLY AS A REFERENCE to see what will eventually be there -- and even then it's not a complete indication -- again just bare bones

---
+++ app/public/jScript/julian/Board.js
@@ -0,0 +1,18 @@
+function Board(boardID, canvas, freeResources, lockedResources) {
+	this.boardID = boardID;
+	this.canvas = canvas;
+	this.freeResources = freeResources;
+	this.lockedResources = lockedResources;
+}
+
+function freeResource() {
+	// move from locked resources to free resources
+}
+
+function lockResource() {
+	// moved from free to locked
+}
+
+function updateCanvas() {
+	// what to update and how
+}
