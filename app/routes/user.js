5fffbe14ef34fd04d186808bd148aea14c5acc95
James Yanyuk <jyanyuk@gmail.com>
2014-11-12 15:49:36 -0500

    Working on authentication

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,22 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    },
-    function(username, password, done) {
-        User.findOne({ username: username }, function(err, user) {
-            if(err) { return done(err); }
-            if(!user) { return done(null, false, { message: 'Incorrect username.' }); }
-            if(!user.validPassword()) { return done(null, false, { message: })}
-        });
-    }
-));
-
 router.get('/', function(req, res) {
     res.render('user', { user: req.user });
 });
