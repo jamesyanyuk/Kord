f25131043bbb0b2db2bb387a45d400aa4456ae54
Matthaus <mjwolff@umass.edu>
2014-11-16 14:48:46 -0500

    User Check

--- app/public/jScript/userDB/regress.js
+++ app/public/jScript/userDB/regress.js
@@ -5,34 +5,53 @@ var pg = require('pg');
 
 var conString = "postgres://cordio:password@localhost/postgres";
 
-var server = http.createServer(function(req, res) {
+var authen = require('authen');
+var resource = require('resource');
 
+function userCheck(username, callback) {
+  pg.connect(connString, function (err, user, done){
+    if(err){callback('Connection error: '+err);
+    done();
+    }
+    else{
+      var query = 'SELECT * FROM users WHERE username' = user.name;
+      client.query(query, function(err, result) {
+
+        if(err){callback('Search error: '+err); 
+        done();
+        }
+        else{callback(undefined);
+        done();
+        }
+    });
+  });
+});
+
+
+//Testing
+//ar server = http.createServer(function(req, res) {
+//
   // get a pg client from the connection pool
-  pg.connect(conString, function(err, client, done) {
-
-    var handleError = function(err) {
+// pg.connect(conString, function(err, client, done) {
+//
+//    var handleError = function(err) {
       // no error occurred, continue with the request
-      if(!err) return false;
+//      if(!err) return false;
 
-      done(client);
-      res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('An error occurred');
-      return true;
-    };
+//      done(client);
+//      res.writeHead(500, {'content-type': 'text/plain'});
+//      res.end('An error occurred');
+//      return true;
+//    };
 
     // record the visit
-    client.query('INSERT INTO visit (date) VALUES ($1)', [new Date()], function(err, result) {
 
-      // handle an error from the query
-      if(handleError(err)) return;
 
-      // get the total number of visits today (including the current visit)
-      client.query('SELECT COUNT(date) AS count FROM visit', function(err, result) {
-
-        // handle an error from the query
+      
+      client.query('INSERT INTO visit (date) VALUES ($1)', [new Date()], function(err, result) {
         if(handleError(err)) return;
-
-        // return the client to the connection pool for other requests to reuse
+      client.query('SELECT COUNT(date) AS count FROM visit', function(err, result) {
+        if(handleError(err)) return;
         done();
         res.writeHead(200, {'content-type': 'text/plain'});
       });
