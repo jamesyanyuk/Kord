b7b7f2f3913d2a813c99945358eaf83acbecd9bb
James Yanyuk <jyanyuk@gmail.com>
2014-11-18 16:32:10 -0500

    Working on authentication

--- app/routes/user.js
+++ app/routes/user.js
@@ -23,7 +23,7 @@ module.exports.initialize = function(passport) {
             User.findOne({ username: username }, function(err, user) {
                 if(err) { return done(err); }
                 if(!user) { return done(null, false, { message: 'Incorrect username.' }); }
-                if(!user.validPassword()) { return done(null, false, { message: })}
+                if(!user.validPassword()) { return done(null, false, { message: 'Incorrect password.' }); }
             });
         }
     ));
