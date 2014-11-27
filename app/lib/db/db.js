2527914f45599d8dd04c25c9b41961689f99389a
James Yanyuk <jyanyuk@umass.edu>
2014-11-27 16:55:37 -0500

    Migrated from node-postgres to node-postgres-pure

--- app/lib/db/db.js
+++ app/lib/db/db.js
@@ -5,7 +5,7 @@
  * */
 
 var http = require('http');
-var pg = require('pg');
+var pg = require('pg.js');
 var connectionString = 'postgres://cnlgnxpqpwvnss:7RWorNu7vA6VtCNH1d_B7Cip_A@ec2-184-73-229-220.compute-1.amazonaws.com:5432/d882b9an2oglco?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
 exports.connectionString = connectionString;
 
