5c7768dbf202a5e9dc5a83f7857365bf1ab0ca5b
James Yanyuk <jyanyuk@umass.edu>
2014-11-27 18:38:58 -0500

    Working on authentication flow

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+// If user's logged in, then redirect to user view
 var isAuth = function(req, res, next) {
     if(req.isAuthenticated())
-        return res.redirect('/user'); // or just next();
+        return res.redirect('/user');
     next();
 }
 
@@ -13,26 +14,31 @@ router.get('/', function(req, res) {
     res.render('index', { title: 'Kord' });
 });
 
-// temp
-router.get('/login', isAuth, function(req, res) {
-    res.render('login', { message: req.flash('loginmessage') || '' });
+router.post('/login', function(req, res, next) {
+    // Check whether the user is already logged in
+    if(req.isAuthenticated())
+        return res.redirect('/user');
+    passport.authenticate('login', function(err, user, info) {
+        // Catch exceptions during authentication
+        if(err) return next(err);
+        // If user hasn't been set, authentication has failed
+        else if(!user) return req.flash('loginmessage');
+        req.login(user, function(err) {
+            // Catch exceptions while establishing login session
+            if(err) return next(err);
+            // Redirect to user view
+            return res.redirect('/user');
+        });
+    })(req, res, next);
 });
 
-router.post('/login', isAuth, passport.authenticate('login', {
-    successRedirect: '/user',
-    failureRedirect: '/',
-    failureFlash: false
-}));
-
-router.get('/register', isAuth, function(req, res) {
-    res.render('register', { message: req.flash('registermessage') || '' });
-});
-
-router.post('/register', isAuth, passport.authenticate('register', {
-    successRedirect: '/',
-    failureRedirect: '/',
-    failureFlash: false
-}));
+router.post('/register', isAuth,
+    passport.authenticate('register', {
+        successRedirect: '/',
+        failureRedirect: '/',
+        failureFlash: false
+    })
+);
 
 // temp
 router.get('/logout', function(req, res) {
