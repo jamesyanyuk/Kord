14ea6c7416791fba84ca6e899daae985344e437e
jkuk <jkuk@umass.edu>
2014-11-30 02:52:58 -0500

    forgot to upload room.js

---
+++ app/public/js/room.js
@@ -0,0 +1,28 @@
+var socket = io.connect('http://' + window.location.host);
+exports.socket = socket;
+// run once
+socket.on('connect', function() {
+    socket.emit('request_memberlist', { roomid: <%= roomid %> });
+    socket.emit('adduser', {
+        roomid: <%= roomid %>,
+        nickname: '<%= nickname %>',
+        userid: <%= userid %>
+    });
+});
+
+socket.on('receive_memberlist', function(data) {
+    for(var prop in data) {
+        console.log('---- ' + prop + ' ---- ' + data[prop]);
+        $('#onlineusers').append($('<li id="' + prop + '">').text(data[prop]));
+    }
+});
+
+socket.on('newconnection', function(data) {
+    console.log('eeeeeee');
+    $('#onlineusers').append($('<li id="' + data.userid + '" %>">').text(data.nickname));
+});
+
+socket.on('disconnection', function(data) {
+    $('#' + data.userid).remove();
+    console.log(data.nickname + ' disconnected.');
+});
