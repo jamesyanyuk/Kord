056baf14307fa234ab77cb58d030ace314f94bd2
James Yanyuk <jyanyuk@umass.edu>
2014-12-02 00:07:30 -0500

    Refactored room.js after removing redundant readEntireRoom call

--- app/lib/sockets/index.js
+++ app/lib/sockets/index.js
@@ -30,8 +30,6 @@ module.exports = function(io) {
                 rooms[data.roomid][data.userid] = data.nickname;
                 idmap[socket.id] = [data.roomid, data.userid];
 
-
-
                 roomdb.readEntireRoom(data.roomid, function(err, room) {
                     if(err) return console.log('Error 34');
 
@@ -52,40 +50,40 @@ module.exports = function(io) {
             }
         );
 
-        socket.on('request_memberlist', function(data) {
-            if (rooms[data.roomid + ''] === undefined) {
-                rooms[data.roomid + ''] = {};
-                rooms[data.roomid + '']['online'] = {};
-                rooms[data.roomid + '']['locked'] = {};
-            }
-            // rooms[data.roomid + '']['online'][data.userid + ''] = data;
+        // socket.on('request_memberlist', function(data) {
+        //     if (rooms[data.roomid + ''] === undefined) {
+        //         rooms[data.roomid + ''] = {};
+        //         rooms[data.roomid + '']['online'] = {};
+        //         rooms[data.roomid + '']['locked'] = {};
+        //     }
+        //     // rooms[data.roomid + '']['online'][data.userid + ''] = data;
+        //
+        //     for (var prop in data) {
+        //         console.log('request: ' + prop + ': ' + data[prop]);
+        //     }
+        //
+        //     socket.emit('receive_memberlist', rooms[data.roomid + '']['online']);
+        // });
 
-            for (var prop in data) {
-                console.log('request: ' + prop + ': ' + data[prop]);
-            }
-
-            socket.emit('receive_memberlist', rooms[data.roomid + '']['online']);
-        });
-
-        socket.on('adduser', function(data) {
-            // Join client to specified roomid channel
-            socket.join('' + data.roomid);
-            var usrid = data.userid;
-            rooms[data.roomid + '']['online'][usrid + ''] = data.nickname;
-            idmap[socket.id] = [data.roomid + '', data.userid + ''];
-            io.to('r' + data.roomid).emit('newconnection', {
-                nickname: data.nickname,
-                userid: data.userid
-            });
-            for (var prop in rooms[data.roomid + '']['online']) {
-                // for (var prop2 in data) {
-                //     console.log('add user: ' + prop2 + ': ' + data[prop2]);
-                // }
-                console.log(prop + ': ' + rooms[data.roomid + '']['online'][prop]);
-                // console.log('.');
-                // console.log('add user: ' + rooms[data.roomid + '']['online']['user']);
-            }
-        });
+        // socket.on('adduser', function(data) {
+        //     // Join client to specified roomid channel
+        //     socket.join('' + data.roomid);
+        //     var usrid = data.userid;
+        //     rooms[data.roomid + '']['online'][usrid + ''] = data.nickname;
+        //     idmap[socket.id] = [data.roomid + '', data.userid + ''];
+        //     io.to('r' + data.roomid).emit('newconnection', {
+        //         nickname: data.nickname,
+        //         userid: data.userid
+        //     });
+        //     for (var prop in rooms[data.roomid + '']['online']) {
+        //         // for (var prop2 in data) {
+        //         //     console.log('add user: ' + prop2 + ': ' + data[prop2]);
+        //         // }
+        //         console.log(prop + ': ' + rooms[data.roomid + '']['online'][prop]);
+        //         // console.log('.');
+        //         // console.log('add user: ' + rooms[data.roomid + '']['online']['user']);
+        //     }
+        // });
 
         socket.on('disconnect', function(data) {
             print_data('disconnect', data);
