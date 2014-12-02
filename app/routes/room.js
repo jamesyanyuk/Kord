7555cfc4e639b15e4a565a96c7a31eeadf7d3bda
James Yanyuk <jyanyuk@umass.edu>
2014-12-01 23:26:14 -0500

    Removed redundant call to readEntireRoom (optimization)

--- app/routes/room.js
+++ app/routes/room.js
@@ -12,31 +12,28 @@ router.get('/:rurl', function(req, res) {
     roomdb.readRoomByUrl(req.params.rurl, function(err, roomidResult) {
         if(err) return res.redirect('/r/404');
 
-        roomdb.readEntireRoom(roomidResult.roomid, function(err, result) {
-            if(err) return res.redirect('/r/404');
-            var room = result;
-            //console.log('Another --->>> ' + result.members.length + ' , ' + room.members.length);
+        if(req.isAuthenticated()) {
+            res.render('room', {
+                nickname: req.user.nickname,
+                userid: req.user.userid,
+                url: room.url,
+                roomid: room.roomid,
+                message: 'Welcome'
+            });
+        } else {
+            // Should clean this up
+            userdb.createUser('Guest' + Math.random().toString().substr(2, 4) + '@' + Math.random().toString().substr(2, 12),
+                Math.random().toString().substr(2, 12), // password
+                -1, // access
+                function(err, guestresult) {
+                    if(err) return res.redirect('/r/404');
+                    else {
+                        roomdb.readModeratorsFor(roomidResult.roomid, function(err, readmodres) {
+                            if(err) return res.redirect('/r/404');
 
-            if(req.isAuthenticated()) {
-                res.render('room', {
-                    nickname: req.user.nickname,
-                    userid: req.user.userid,
-                    url: room.url,
-                    roomid: room.roomid,
-                    message: 'Welcome'
-                });
-            } else {
-                // Should clean this up
-                userdb.createUser('Guest' + Math.random().toString().substr(2, 4) + '@' + Math.random().toString().substr(2, 12),
-                    Math.random().toString().substr(2, 12), // password
-                    -1, // access
-                    function(err, guestresult) {
-                        if(err) return res.redirect('/r/404');
-                        else {
-                            if(room['moderators'].length === 0){
-                                roomdb.joinRoomMember(room.roomid, guestresult.userid, function(err, joinresult) {
+                            if(readmodres.length === 0){
+                                roomdb.joinRoomMember(roomidResult.roomid, guestresult.userid, function(err, joinresult) {
                                     if(err) return res.redirect('/r/404');
-                                    //room['members'].push(guestresult);
 
                                     res.render('room', {
                                         nickname: guestresult.nickname,
@@ -53,11 +50,11 @@ router.get('/:rurl', function(req, res) {
                                     message: 'Welcome'
                                 });
                             }
-                        }
+                        });
                     }
-                );
-            }
-        });
+                }
+            );
+        }
     });
 });
 
